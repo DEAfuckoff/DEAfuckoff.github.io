17a3f895d6f63546911a3bb2a7625b191b609a74
DEAfuckoff <125322709+DEAfuckoff@users.noreply.github.com>
2023-03-05 20:28:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,40 +4,38 @@ var img = new Image();
 img.src = "bitcoin.png";
 var particles = [];
 
-function Particle() {
-  this.x = Math.random() * canvas.width;
-  this.y = 0;
-  this.vx = -2 + Math.random() * 4;
-  this.vy = 4 + Math.random() * 4;
-  this.gravity = 0.2;
+function createParticle() {
+  var particle = document.createElement("div");
+  particle.style.position = "absolute";
+  particle.style.top = "0";
+  particle.style.left = Math.random() * window.innerWidth + "px";
+  particle.style.width = "50px";
+  particle.style.height = "50px";
+  particle.style.backgroundImage = "url('bitcoin.png')";
+  particle.style.backgroundSize = "contain";
+  particle.style.backgroundRepeat = "no-repeat";
+  document.body.appendChild(particle);
+  particles.push(particle);
 }
 
-Particle.prototype.draw = function() {
-  ctx.drawImage(img, this.x, this.y, 50, 50);
-};
-
-Particle.prototype.update = function() {
-  this.x += this.vx;
-  this.y += this.vy;
-  this.vy += this.gravity;
-
-  if (this.y > canvas.height) {
-    this.y = 0;
-    this.vy = 4 + Math.random() * 4;
-  }
-};
-
-for (var i = 0; i < 50; i++) {
-  particles.push(new Particle());
+function animateParticle(particle) {
+  anime({
+    targets: particle,
+    translateY: [
+      { value: window.innerHeight, duration: 2000, easing: "linear" },
+    ],
+    rotate: {
+      value: 360,
+      duration: 1000,
+      easing: "easeInOutSine",
+    },
+    complete: function(anim) {
+      particle.parentNode.removeChild(particle);
+    },
+  });
 }
 
-function loop() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (var i = 0; i < particles.length; i++) {
-    particles[i].draw();
-    particles[i].update();
-  }
-  requestAnimationFrame(loop);
-}
-
-loop();
+setInterval(function() {
+  createParticle();
+  animateParticle(particles[particles.length - 1]);
+}, 100);
