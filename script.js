7544a38eb5f73210688f17a7da8f04fc137efccb
DEAfuckoff <125322709+DEAfuckoff@users.noreply.github.com>
2023-03-05 20:35:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,36 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var img = new Image();
-img.src = "bitcoin.png";
-var particles = [];
+const numberOfBitcoin = 30;
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
 
-function createParticle() {
-  var particle = document.createElement("div");
-  particle.style.position = "absolute";
-  particle.style.top = "0";
-  particle.style.left = Math.random() * window.innerWidth + "px";
-  particle.style.width = "50px";
-  particle.style.height = "50px";
-  particle.style.backgroundImage = "url('bitcoin.png')";
-  particle.style.backgroundSize = "contain";
-  particle.style.backgroundRepeat = "no-repeat";
-  document.body.appendChild(particle);
-  particles.push(particle);
-}
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-function animateParticle(particle) {
-  anime({
-    targets: particle,
-    translateY: [
-      { value: window.innerHeight, duration: 4000, easing: "linear" },
-    ],
-    rotate: {
-      value: 360,
-      duration: 1000,
-      easing: "easeInOutSine",
-    },
-    complete: function(anim) {
-      particle.parentNode.removeChild(particle);
-    },
+const bitcoinImage = new Image();
+bitcoinImage.src = "bitcoin.png";
+
+const bitcoins = [];
+for (let i = 0; i < numberOfBitcoin; i++) {
+  bitcoins.push({
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    size: Math.random() * 50 + 50,
+    speed: Math.random() * 2 + 1
   });
 }
 
-setInterval(function() {
-  createParticle();
-  animateParticle(particles[particles.length - 1]);
-}, 100);
+function draw() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  bitcoins.forEach(bitcoin => {
+    context.drawImage(bitcoinImage, bitcoin.x, bitcoin.y, bitcoin.size, bitcoin.size);
+    bitcoin.y += bitcoin.speed;
+    if (bitcoin.y > canvas.height) {
+      bitcoin.y = 0 - bitcoin.size;
+      bitcoin.x = Math.random() * canvas.width;
+    }
+  });
+}
+
+anime({
+  duration: Infinity,
+  update: draw
+});
